e8c3cf2ed55b57410a19c6336e86edf9aa44b5a3
chrstj <c.stjernberg@hotmail.com>
2024-09-18 20:58:22 +0200

    logo

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,13 +1,10 @@
 import Link from "next/link";
-import { BsFlag } from "react-icons/bs";
 
 function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-10xl mx-auto'>
       <Link href={"/"} className='flex gap-1'>
-        <h1>
-          <BsFlag />
-        </h1>
+        <h1>GEO-QUIZ🌏</h1>
       </Link>
     </div>
   );
