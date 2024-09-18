3367765600e5cf36aa57bbbf4b7b3b15859e7d81
chrstj <c.stjernberg@hotmail.com>
2024-09-18 21:13:45 +0200

    logo change

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-10xl mx-auto'>
       <Link href={"/"} className='flex gap-1'>
-        <h1>GEO-QUIZ🌏</h1>
+        <h1 className='rounded-lg bg-[#1f2937] text-white p-2'>GEO-QUIZ🌏</h1>
       </Link>
     </div>
   );
