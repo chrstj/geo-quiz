9d6f1d5f6d567a02d610b6b7eb6120bfe6939360
chrstj <c.stjernberg@hotmail.com>
2024-10-19 16:06:20 +0200

    Removed images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/app/components/Header";
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
